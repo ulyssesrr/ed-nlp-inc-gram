fc1859627c46434b00b32395f9a195ea27e86429
Ulysses Rangel Ribeiro <ulyssesrr@gmail.com>
2015-09-02 19:08:58 -0300

    Sample matching

--- inc-gram.py
+++ inc-gram.py
@@ -6,9 +6,14 @@ import gc
 import resource
 import re
 import logging
+import pickle
+import math
 import time
 import itertools
 from datetime import timedelta
+
+from multiprocessing import Pool
+import functools
 
 from optparse import OptionParser
 
@@ -16,6 +21,7 @@ import numpy as np
 
 from scipy import sparse as sp
 
+import sklearn
 from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -42,9 +48,9 @@ if sys.stdout.encoding == None:
 
 (options, args) = parser.parse_args()
 
-#if len(args) == 0:
-	#parser.print_help()
-	#sys.exit()
+if len(args) == 0:
+	parser.print_help()
+	sys.exit()
 
 class ClusteringSelectStrategy:
 	
@@ -74,29 +80,36 @@ class ClusteringSelectStrategy:
 		
 class ClusteringSelectStrategy2:
 	
-	def select_grammar(self, vectors, candidates_simple, sorted_grams_idx, gram_rank):
+	def select_grammar(self, vectors, candidates, sorted_grams_idx, gram_rank):
 		#clusterer = KMeansClusterer(2, nltk.cluster.util.euclidean_distance)#, initial_means=means) 
 		#clusters = clusterer.cluster(vectors.todense(), True, trace=True) 
 		#print('Clusters:', clusters )
 		#print('Means:', clusterer.means())
 		log.info("Clustering...")
-		n_clusters = 2
+		n_clusters = math.floor(len(candidates)/2)
 		kmeans = KMeans(n_clusters=n_clusters, n_jobs=-1)
 		kmeans.fit(gram_rank)
 		order_centroids = kmeans.cluster_centers_.argsort()[:, ::-1]
-		print(order_centroids)
-		for i in range(2):
-			print("Cluster %d:" % i, end='')
-			for ind in order_centroids[i, :2]:
-				print(candidates_simple[ind])
+		#print(order_centroids)
+		#for i in range(n_clusters):
+			#print("Cluster %d:" % i, end='')
+			#for ind in order_centroids[i, :2]:
+				#print(candidates_simple[ind])
 				
 		log.info("Getting centers...")
 		for label in range(n_clusters):
 			label_idxs = np.where(kmeans.labels_ == label)
-			c_idx = euclidean_distances(vectors[label_idxs], kmeans.cluster_centers_[i]).argmin(axis=0)[0]
-			print(c_idx)
-			nearest = vectors[label_idxs][c_idx]
-			print("Nearest %d: " % (label), nearest.todense(), np.array(candidates_simple)[label_idxs][c_idx])
+			#print("CENTER", kmeans.cluster_centers_[label])
+			if label_idxs[0].shape[0] > 0:
+				#print(label_idxs, )
+				#print(gram_rank[label_idxs])
+				c_idx = euclidean_distances(gram_rank[label_idxs], kmeans.cluster_centers_[label]).argmin(axis=0)[0]
+				print(c_idx)
+				nearest = vectors[label_idxs][c_idx]
+				print("Nearest %d: " % (label), nearest.todense(), np.array(candidates_simple)[label_idxs][c_idx])
+				abs_idx = np.arange(len(gram_rank))[label_idxs][c_idx]
+				print("ABS Nearest %d: " % (label), nearest.todense(), candidates_simple[abs_idx])
+		print("END")
 
 
 
@@ -124,9 +137,7 @@ with open("input.txt") as f:
 	inputs = [i.replace("\n", "").lower().split(";") for i in text]
 	log.info(inputs)
 
-#dataset_folder = args[0]
-
-lexicon = Lexicon("/home/ulysses/Apps/Unitex3.1beta/Portuguese (Brazil)/Dela/")
+lexicon = Lexicon("/home/ulysses/Applications/Unitex3.1beta/Portuguese (Brazil)/Dela/")
 
 def get_candidates(sentences):
 	candidates_simple = set()
@@ -162,28 +173,33 @@ def get_candidates(sentences):
 
 sentences = [s[1] for s in inputs]
 
-log.info("Loading Mac-Morpho Tagged Sents...")
-tsents = list(mac_morpho.tagged_sents())
+try:
+	tagger2 = pickle.load(open( "tagger.pickle", "rb" ))
+	log.info("Loaded pre-trained tagger.")
+except:
+	log.info("Loading Mac-Morpho Tagged Sents...")
+	tsents = list(mac_morpho.tagged_sents())
 
 
-def simplify_tag(t):
-	if "+" in t:
-		t = t[t.index("+")+1:]
-	
-	if t == "ART":
-		return "DET"
-	
-	return t
+	def simplify_tag(t):
+		if "+" in t:
+			t = t[t.index("+")+1:]
+		
+		if t == "ART":
+			return "DET"
+		
+		return t
 
-log.info("Simplifyng POS Tags...")
-tsents = [[(w.lower(),simplify_tag(t)) for (w,t) in sent] for sent in tsents if sent]
+	log.info("Simplifyng POS Tags...")
+	tsents = [[(w.lower(),simplify_tag(t)) for (w,t) in sent] for sent in tsents if sent]
 
-train = tsents
-test = tsents[:300]
-log.info("Training POS Taggers...")
-tagger0 = nltk.DefaultTagger('N')
-tagger1 = nltk.UnigramTagger(train, backoff=tagger0)
-tagger2 = nltk.BigramTagger(train, backoff=tagger1)
+	train = tsents
+	test = tsents[:300]
+	log.info("Training POS Taggers...")
+	tagger0 = nltk.DefaultTagger('N')
+	tagger1 = nltk.UnigramTagger(train, backoff=tagger0)
+	tagger2 = nltk.BigramTagger(train, backoff=tagger1)
+	pickle.dump(tagger2, open( "tagger.pickle", "wb" ) )
 
 #log.info("Evaluate tagger")
 #print(tagger2.evaluate(test))
@@ -193,14 +209,68 @@ tagger2 = nltk.BigramTagger(train, backoff=tagger1)
 #fd = nltk.FreqDist(tags)
 #print(fd.keys())
 
+dataset_folder = args[0]
+
+log.info("Loading dataset %s (encoding=%s)..." % (dataset_folder, options.encoding))
+dataset = sklearn.datasets.load_files(dataset_folder, encoding=options.encoding, decode_error='strict')
+log.info("[OK]")
+
+log.info("Preprocessing Dataset: Cleaning HTML tags")
+compiled_re = re.compile("<[^<]+?>")
+dataset.data = [compiled_re.sub('', row) for row in dataset.data]
+
+log.info("Preprocessing Dataset: to lowercase...")
+dataset.data = [row.lower() for row in dataset.data[:1000]]
+
+log.info("Tokenizing sentences in text...")
+sent_tokenizer = nltk.data.load('tokenizers/punkt/portuguese.pickle')
+with Pool() as p:
+	dataset_sentences = list(itertools.chain.from_iterable(p.map(sent_tokenizer.tokenize, dataset.data)))
+print(dataset_sentences[0])
+log.info("[%d]" % (len(dataset_sentences)))
+
+log.info("Tokenizing words in sentences...")
+tokenizer = re.compile('\w+')
+with Pool() as p:
+	tokenized_sents = list(p.map(tokenizer.findall, dataset_sentences))
+	log.info("[OK]")
+	log.info("Tagging dataset...")
+	tagged_dataset = p.map(tagger2.tag, tokenized_sents)
+	log.info("Done.")
+print(tagged_dataset[0])
+
+def find_sentence_match(tagged_sent, grammar):
+	tags = [tag for word, tag in tagged_sent]
+	if " ".join(grammar) in " ".join(tags):
+		return tagged_sent
+	return None
+
+def find_dataset_match(tagged_dataset, grammar):
+	with Pool() as p:
+		f = functools.partial(find_sentence_match, grammar=grammar)
+		return list(filter(lambda r : r != None, p.map(f, tagged_dataset)))
+
 tokenizer = re.compile('\w+')
 for input_id, s in enumerate(sentences):
 	log.info("Sentence: %s" % (s))
 	candidates_simple, candidates_med, candidates_full = get_candidates([s]) 
+	candidates_simple, candidates_med, candidates_full = list(candidates_simple), list(candidates_med), list(candidates_simple)
+	samples = []
+	for i, cand_simple in enumerate(candidates_simple):
+		sample = find_dataset_match(tagged_dataset, cand_simple)
+		print(i, cand_simple, len(sample))
+		if len(sample) == 0:
+			del candidates_simple[i]
+			del candidates_med[i]
+			del candidates_full[i]
+		else:
+			samples += [sample]
+			print(sample[0])
+	
+	candidates_simple, candidates_med, candidates_full = np.array(candidates_simple), np.array(candidates_med), np.array(candidates_simple)
 	#print(candidates_simple)
 	tagged_sent = [tagger2.tag([w])[0][1] for w in tokenizer.findall(s)]
 	#print(s, tagged_sent)
-	candidates_simple = np.array(list(candidates_simple))
 	tagged_sent = np.array(tagged_sent)
 	gram_acc = candidates_simple == tagged_sent
 	#print(gram_acc, candidates_simple, tagged_sent)
@@ -238,7 +308,7 @@ for input_id, s in enumerate(sentences):
 	#print(top_gram)
 	sorted_grams_idx = np.argsort(-gram_rank, axis=0)
 	
-	selection_strategy = ClusteringSelectStrategy()
+	selection_strategy = ClusteringSelectStrategy2()
 	selection_strategy.select_grammar(X_vect, candidates_simple, sorted_grams_idx, gram_rank)
 	
 	log.info("%s: Writing results..." % (inputs[input_id]))
